467e83ebe4dd746485cf1eb01c4f0de0411106ca
Uma Maheswari <umachinnam99@gmail.com>
2026-03-04 17:52:55 -0500

    postgres schema with job lifecycle tracking successfully completed

--- proto/scheduler_pb2_grpc.py
+++ proto/scheduler_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import scheduler_pb2 as scheduler__pb2
+from proto import scheduler_pb2 as scheduler__pb2
 
 GRPC_GENERATED_VERSION = '1.78.0'
 GRPC_VERSION = grpc.__version__
